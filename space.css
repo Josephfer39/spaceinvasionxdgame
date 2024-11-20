a42e7edb9a2c8dbafd9f20020b6a4a2a038f455f
Josephfer39 <josephf0793@gmail.com>
2024-11-20 17:16:28 -0500

    Create space.css

---
+++ space.css
@@ -0,0 +1,21 @@
+usng System;
+namesapace CodeSample{
+    classProgramming{
+         staticvoidMain(string[]args){
+            int A < 6
+
+            if (A < 6) 
+{
+  Console.WriteLine("you can play the game with some restrictions");
+}
+            elseif (A > 5) 
+{
+  Console.WriteLine("You can enjoy this game easy without restrictions");
+}
+            else  
+{
+  Console.WriteLine("print a valid number");
+}
+         }
+    }
+}      
